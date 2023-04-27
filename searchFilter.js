ee7fcce1d18a206685f39f811da21da17277b781
damian <damian_daley@yahoo.com>
2023-04-27 01:51:23 -0500

    added more java script

---
+++ searchFilter.js
@@ -0,0 +1,59 @@
+function search()
+
+{
+
+    
+
+    const searchbox = document.getElementById("search-box").value.toLowerCase();
+    const recipeItems = document.getElementById("recipesAll");
+    const recipes = document.querySelectorAll(".recipeItem");
+    const recipeName = document.getElementsByTagName("h4");
+	let searchResults = document.getElementById("sResults")
+
+    let matchCount = 0;
+
+    for(var i=0; i < recipeName.length; i++)
+    {
+        let match = recipes[i].getElementsByTagName('h4')[0]; //get the first h4 element, index[0]
+
+        if(match)
+        {
+          //  alert(" match.innerHTML : " +  match.innerHTML);
+            
+            let textvalue = ( match.textContent || match.innerHTML).toLowerCase();
+            
+          //  alert("textValue : " + textvalue);
+            
+            if(textvalue.indexOf(searchbox) > -1)
+            {
+				matchCount+=1;
+                recipes[i].style.display = "";
+            //    alert(searchbox + "is in " + textvalue);
+			
+            }
+            else
+            {
+				
+				
+                recipes[i].style.display = "none";
+              //  alert(searchbox + "is NOT in " + textvalue);
+              //  alert(searchbox.value + "is in " + textvalue);
+			  //document.getElementsByClassName("main").innerHTML = "<div><h2> Sorry, No match Found!</h2></div>";
+            }
+        }
+		
+    }
+	if(matchCount == 0)
+	{
+		searchResults.style.display = "";
+		searchResults.innerHTML = "Sorry, No match Found!";
+	}
+	else
+	{
+		searchResults.style.display = "none";
+	}
+		//alert("No match found");
+	
+	//alert("Match Count:" + matchCount);
+	
+}
